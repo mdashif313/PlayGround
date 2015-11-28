4ab3e99c67984bc401bdc602b4925344f41b4515
mdashif313 <mdashifalnowajesh@gmail.com>
2015-11-28 11:11:50 -0600

    single file upload

--- src/com/ashif/play/Client.java
+++ src/com/ashif/play/Client.java
@@ -7,19 +7,34 @@ package com.ashif.play;
  */
 
 
-import java.net.InetSocketAddress;
+import java.io.*;
+import java.net.*;
+import java.nio.ByteBuffer;
 import java.nio.channels.*;
+import java.util.*;
 
 
 
 public class Client {
 
-    public static int port = 2222;
-    public static String host = "localhost";
+    //Constants
+    public static final int PORT = 2222;
+    public static String HOST = "localhost";
+    public static final String CLIENT_DIRECTORY="d:/temp/client_2/";
     
     public static void main(String[] args) {
         
+        while (true) {            
+            System.out.println("1.Upload File");
+            System.out.println("2.Download File");
+            System.out.print("Give your choice : ");
+            //taking input from user
+            Scanner inFromUser = new Scanner(System.in);
+            int choice = inFromUser.nextInt();
         
+            if(choice == 1)
+                FileUploader();
+        }
         
         
     }
@@ -29,9 +44,66 @@ public class Client {
     public static void FileUploader(){
         
         try {
-            SocketChannel socketChannel = SocketChannel.open();
-            socketChannel.bind(new InetSocketAddress(host, port));
+            //initializing connection with server
+            SocketChannel clientChannel = SocketChannel.open();
+            clientChannel.connect(new InetSocketAddress(HOST,PORT));
             
+            String pingToServer = "receive";
+            //converting ping string to bytearray
+            byte[] pingBytes = pingToServer.getBytes("UTF-8");
+            
+            //sending ping to server
+            ByteBuffer pingBuffer=ByteBuffer.wrap(pingBytes);
+            clientChannel.write(pingBuffer);
+            
+            //taking input from user
+            Scanner inFromUser = new Scanner(System.in);
+            System.out.print("Number of files : ");
+            int numofFiles=inFromUser.nextInt();
+            
+            String quantofFiles=numofFiles+"";
+            //converting ping string to bytearray
+            byte[] quantBytes = quantofFiles.getBytes("UTF-8");
+            
+            //sending ping to server
+            ByteBuffer quantBuffer=ByteBuffer.wrap(quantBytes);
+            clientChannel.write(quantBuffer);
+            
+            ArrayList<String> listofFiles= new ArrayList<String>();
+            
+            //taking the name of files from user
+            inFromUser.nextLine();
+            for(int i=0;i<numofFiles;i++){
+                System.out.print("File name : ");
+                String str=inFromUser.nextLine();
+                listofFiles.add(str);
+            }
+            
+            //sending files to server
+            for(int i=0; i<numofFiles; i++){                
+                String fileName = listofFiles.get(i);
+                File fileToSend=new File(CLIENT_DIRECTORY+fileName);
+                
+                //converting filename string to bytearray
+                byte[] nameBytes = fileName.getBytes("UTF-8");
+            
+                //sending filename to server
+                ByteBuffer nameBuffer=ByteBuffer.wrap(nameBytes);
+                clientChannel.write(nameBuffer);
+                
+                FileChannel sbc=FileChannel.open(fileToSend.toPath());
+                ByteBuffer buff=ByteBuffer.allocate(10000000);
+                int bytesread=sbc.read(buff);
+                
+                while(bytesread != -1){
+                    buff.flip();
+                    clientChannel.write(buff);
+                    buff.compact();
+                    bytesread=sbc.read(buff);
+                }
+                System.out.println(fileName+" has been sent.");
+                Thread.sleep(300);
+            }
             
         } catch (Exception e) {
             System.out.println("Exception inside Client FileUploader "+e);
--- src/com/ashif/play/Server.java
+++ src/com/ashif/play/Server.java
@@ -1,14 +1,112 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.ashif.play;
+
+import java.io.*;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+
 
 /**
  *
  * @author Ashif
  */
+
+
+
+
 public class Server {
+
+    //Constants
+    public static int PORT = 2222;
+    public static final String SERVER_DIRECTORY="d:/temp/server/";
     
+    public static void main(String[] args) throws IOException{
+        
+        //initializing our server
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.socket().bind(new InetSocketAddress(PORT));
+        
+        while (true) {            
+            System.out.println("Waiting for Client");
+            
+            SocketChannel clientChannel= serverSocketChannel.accept();
+            System.out.println("Accepting Client request");
+            
+            ByteBuffer pingbuff=ByteBuffer.allocate(500);
+            clientChannel.read(pingbuff);
+            byte[] pingbyte=new byte[500];
+
+            int i=0;       
+            int position=pingbuff.position();
+           
+            while(i<position){
+                pingbyte[i]=pingbuff.get(i);
+                i++;
+            }
+            
+            String ping=new String(pingbyte,0,position);            
+            
+            //Calling appropriate handler for client request
+            if (ping.equalsIgnoreCase("receive")) {
+                Receiver(clientChannel);
+            }
+        }
+    }
+    
+    
+    public static void Receiver(SocketChannel clientChannel){
+        try {
+            ByteBuffer numBuffer=ByteBuffer.allocate(500);
+            clientChannel.read(numBuffer);
+            byte[] numByte=new byte[500];
+
+            int j=0;       
+            int pos=numBuffer.position();
+           
+            while(j<pos){
+                numByte[j]=numBuffer.get(j);
+                j++;
+            }
+            
+            String number=new String(numByte,0,pos);
+            int numofFiles=Integer.parseInt(number);
+            
+            
+            //receiving files from client
+            for(int i=0; i<numofFiles; i++){
+                            
+                ByteBuffer namebuff=ByteBuffer.allocate(500);
+                clientChannel.read(namebuff);
+                byte[] namebyte=new byte[500];
+
+                int k=0;       
+                int position=namebuff.position();
+           
+                while(k<position){
+                    namebyte[k]=namebuff.get(k);
+                    k++;
+                }
+                
+                String filename=new String(namebyte,0,position); 
+                File file=new File(SERVER_DIRECTORY+filename);
+
+                ByteBuffer bb=ByteBuffer.allocate(10000000);
+                int bytesRead=clientChannel.read(bb);
+                FileOutputStream bout =new FileOutputStream(file);
+                FileChannel sbc=bout.getChannel();
+
+                while(bytesRead != -1){
+                    bb.flip();
+                    sbc.write(bb);
+                    bb.compact();
+                    bytesRead=clientChannel.read(bb);
+                }
+                
+                System.out.println(filename+"has been received");
+            }
+
+        } catch (Exception e) {
+        }
+    }
 }
